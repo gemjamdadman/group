d5cec721472f91a660dd593fe9ae37c3c370c96c
Andrew Lamb <andrew.lamb27@btinternet.com>
2017-01-27 12:38:58 +0000

    adding index test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!Doctype html>
+<head>
+  <link rel = "stylesheet" type = "text/css" href = "group.css">
+</head>
+<title>
+  Codex Books
+</title>
+  <body>
+
+<?php require (head.php); ?>
+
+   <div class = "centerContainer">
+
+      <p class = "nav">nav side bar</p>
+      <p class = "display">bloody hell</p>
+<!-- this bit changes - to hold seach results etc
+      <div class = "centerConsole">
+        main images - sales banners etc
+-->
+      <p class = "sidebar">suggested items</p>
+  </div>
+
+<?php  require (foot.php);?>
+
+</body>
+</html>
