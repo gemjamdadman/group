a791da797d7bbfdc522d36e71ce4aeec95d55b93
MichaelPham03 <michaelpham03@gmail.com>
2017-02-03 12:52:59 +0000

    Add files via upload

--- dataSearch.php
+++ dataSearch.php
@@ -2,6 +2,24 @@
 require 'head.php';
 ?>
 
+<?php
+$server = '194.81.104.22';
+$username = 'gp_general_1617';
+$password = 'general';
+
+$schema = 'db_general_1617';
+
+$pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$results = $pdo->query('SELECT * FROM BOOK');
+
+foreach ($results as $row)
+{
+ echo '<p>' . $row['TITLE'] . '</p>';
+}
+
+?>
+
 <div class = "dataSearch">
 <p>
 linked results here
