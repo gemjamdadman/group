a9d8187c78d214a7e3af05a09ffe9b6ca977d935
Ben742 <bentherat742@hotmail.co.uk>
2017-02-08 12:15:53 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,7 @@
 <?php
-
-$server = '194.81.104.22';//creates a variable that holds the server name
-$username = 'gp_general_1617';//creates a variable that holds the username 
-$password = 'db_general_1617';//creaes  a variable that holds the password
-//The name of the schema we created earlier in MySQL workbench
-//If this schema does not exist you will get an error!
+$server = '194.81.104.22';
+$username = 'gp_general_1617';
+$password = 'general';
 $schema = 'db_general_1617';
-$pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password,
-[ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$pdo = new PDO('mysql:dbname=' . $schema . ';host=' . $server, $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 ?>
