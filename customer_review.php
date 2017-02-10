da764d510eb7cabb40b857c1aa23237b8c422344
Ben742 <bentherat742@hotmail.co.uk>
2017-02-10 08:31:26 +0000

    Update customer_review.php

--- customer_review.php
+++ customer_review.php
@@ -12,25 +12,20 @@ require ('db.php');
 
     <h2>Customer review</h2>
     <!-- creates heading -->
-    First name: <!-- creates heading -->
-     <input name="firstname" type="text">
+    USER_EMAIL: <!-- creates heading -->
+     <input name="USER_EMAIL" type="text">
     <!-- creates a input to put data inside-->
-     last name: <!-- creates heading -->
-     <input name="surname" type="text">
+     BOOK_ISBN: <!-- creates heading -->
+     <input name="BOOK_ISBN" type="text">
     <!-- creates a input to put data inside-->
-     Email:<!-- creates heading -->
-     <input name="email" type="text"><!-- creates a input to put data inside-->
-    <br />
-    Product Description:<br />
+    TEXT_REVIEW:<br />
     <!-- creates heading -->
-     
-
-    <textarea cols="80" name="message_text" rows="10"></textarea>
+    <textarea cols="80" name="TEXT_REVIEW" rows="10"></textarea>
     <!-- creates a input to put data inside-->
     <br />
-    Product rating:<br />
+    REVIEW_RATING:<br />
     <!-- creates heading -->
-     <select name="rating">
+     <select name="REVIEW_RATING">
       <!-- creates a select for rating -->
 
 
@@ -68,21 +63,16 @@ require ('db.php');
   <?php
 
 if (isset($_POST['customer_submit']))
-   { //if the button is clicked the code below runs
-   $firstname = $_POST['firstname']; //once clicked  varibles will post
-   $surname = $_POST['surname']; //once clicked  varibles will post
-   $email = $_POST['email']; //once clicked  varibles will post
-   $message_num = $_POST['message_num']; //once clicked  varibles will post
-   $message_text = $_POST['message_text']; //once clicked  varibles will post
-   $rating = $_POST['rating']; //once clicked  varibles will post
-   $date = date("Y-m-d H:i:s"); //insert date and puts it in the format
-
-   // insert data into messages and the names of the names in the database
-   // the values of the variables will insert into database
-
-   $pdo->query('INSERT INTO messages (firstname, surname, email,  message_num, message_text, rating, date)
-          VALUES ("' . $firstname . '", "' . $surname . '", "' . $email . '", "' . $message_num . '", "' . $message_text . '","' . $rating . '","' . $date . '")');
-   }
-
-?>
+ $stmt = $pdo->prepare('INSERT INTO person (USER_EMAIL, BOOK_ISBN, TEXT_REVIEW, REVIEW_RATING, REVIEW_DATE)
+ VALUES (:USER_EMAIL, :BOOK_ISBN, :TEXT_REVIEW, :REVIEW_RATING, :REVIEW_DATE)
+');
+$criteria = [
+ 'USER_EMAIL' => $_POST['USER_EMAIL'],
+ 'BOOK_ISBN' => $_POST['surname'],
+ 'TEXT_REVIEW' => $_POST['email'],
+ 'REVIEW_RATING' => $_POST['birthday']
+ 'REVIEW_DATE' => date("Y-m-d H:i:s");
+];
+$stmt->execute($criteria);
+  ?>
 <?php require 'foot.php'; ?>
